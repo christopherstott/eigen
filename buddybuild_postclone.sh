06156a5388fce22f89c2cf2079457e4a9d815b4c
Christopher Stott <christopherstott@gmail.com>
2015-12-01 09:31:56 -0800

    SKip bundler

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
-echo password | sudo -S gem install bundler
-bundle install
-make oss
+echo password | sudo -S gem install cocoapods-keys
+pod keys set "ArtsyAPIClientSecret" "3a33d2085cbd1176153f99781bbce7c6" Artsy
+pod keys set "ArtsyAPIClientKey" "e750db60ac506978fc70"
+pod keys set "ArtsyFacebookAppID" "-"
+pod keys set "ArtsyTwitterKey" "-"
+pod keys set "ArtsyTwitterSecret" "-"
+pod keys set "ArtsyTwitterStagingKey" "-"
+pod keys set "ArtsyTwitterStagingSecret" "-"
+pod keys set "SegmentProductionWriteKey" "-"
+pod keys set "SegmentDevWriteKey" "-"
+pod keys set "AdjustProductionAppToken" "-"
